382d782210e3fbce848d52f20e35e004c31d7224
Esfahani <esfahanimichael@gmail.com>
2019-03-03 10:22:08 -0800

    adding email information

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'esfahanimichael@gmail.com';
+$siteTitle = 'website responses..';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
